c3dd466399f9da987de870a020b2e81aa7dcdde6
programminghouse588 <thehouseofprogramming588@gmail.com>
2024-04-06 00:41:45 +0600

    login page added

---
+++ src/pages/Shared/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar/Navbar";
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h1>Logins page</h1>
+        </div>
+    );
+};
+
+export default Login;
